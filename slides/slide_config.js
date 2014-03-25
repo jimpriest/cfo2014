6d2dc417d2fcdaeba25d24b5e0176bc29094e3cc
Jim Priest <priest@thecrumb.com>
2014-03-24 20:00:55 -0400

    Added slides - cleaned up old files.

---
+++ slides/slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Digging Into The Developer Toolbox',
+    subtitle: 'Work Smarter. Not Harder.',
+    eventInfo: {
+      title: 'cf.Objective',
+      date: '2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: false, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro',
+      'Yanone Kaffeesatz'
+    ],
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Jim Priest',
+    company: '',
+    gplus: '',
+    twitter: '@thecrumb',
+    www: 'http://thecrumb.com',
+    github: 'http://github.com/jimpriest'
+  }]
+};
+
